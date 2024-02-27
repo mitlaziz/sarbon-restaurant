6cf1ea79ac9af18ccfc34a7b567a30e88665cfed
Max <lazizyuldoshev93@gmail.com>
2024-02-27 14:29:20 +0900

    feat: Express integrations

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,7 @@
 console.log("HELLO WORLD");
+
+function gerReverse(input: string): string {
+    return input.split('').reverse().join('');
+}
+
+console.log(gerReverse("Uzbekistan")); // Output: natsikebzu
