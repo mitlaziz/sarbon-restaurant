559ace2c0b9a355380c5a766ed4d39e61a37c6bf
Max <lazizyuldoshev93@gmail.com>
2024-02-29 00:58:17 +0900

    fix: modify routing system and  controllers

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import path from "path";
 import router from "./router";
+import routerAdmin from './routerAdmin';
 
 /** 1-ENTRANCE **/
 const app  = express();
 console.log("__dirname:",__dirname);
-
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -18,6 +18,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
-app.use('/', router);  //Middleware Design Pattern
+app.use("/admin", routerAdmin);// backend server site rendering(bssr): EJS dan use
+app.use('/', router);  // SPA: REACT
 
 export default app;    // module.exports = app   -bu common JS da 
--- src/router.ts
+++ src/router.ts
@@ -2,19 +2,11 @@ import express, {Request, Response} from "express";
 const router = express.Router();
 import memberController from './controllers/member.controller';
 
-router.get("/", memberController.goHome);
-// router.get("/", (req:Request, res:Response) => {
-//     res.send("Home Page");
-// });
+// router.get("/", memberController.goHome);
 
-router.get("/login", memberController.getLogin);
-// router.get("/login", (req:Request, res:Response) => {
-//     res.send("Login Page");
-// });
+// router.get("/login", memberController.getLogin);
 
-router.get("/signup", memberController.getSignup);
-// router.get("/signup", (req:Request, res:Response) => {
-//     res.send("Signup Page");
-// });
+// router.get("/signup", memberController.getSignup);
+
 
 export default router;
