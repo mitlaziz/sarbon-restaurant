992e8f6397e7756b6befff2659ed24075125920a
Max <lazizyuldoshev93@gmail.com>
2024-02-29 03:00:27 +0900

    feat: task H

--- src/train.ts
+++ src/train.ts
@@ -1,8 +1,23 @@
 console.log("HELLO WORLD");
 
-// Task H
-function gerReverse(input: string): string {
-    return input.split('').reverse().join('');
+//Task H
+
+function getPositive(arr: number[]) {
+    let result = "";
+    for (let num of arr) {
+        if (num > 0) {
+            result += num.toString();
+        }
+    }
+    return result;
 }
 
-console.log(gerReverse("Uzbekistan")); // Output: natsikebzu
+console.log(getPositive([3, -6, 9]));  // Output: "12"
+
+
+// // Task H
+// function gerReverse(input: string): string {
+//     return input.split('').reverse().join('');
+// }
+
+// console.log(gerReverse("Uzbekistan")); // Output: natsikebzu
