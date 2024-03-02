ed38ab57a9aa9660087421d956eaca5f53817375
Max <lazizyuldoshev93@gmail.com>
2024-03-02 11:30:44 +0900

    feat: create project standards

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -5,6 +5,7 @@ import MemberService from "../models/Member.service";
 const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
     try {
+      console.log("goHome");  //loyiha standarti.mantiq yetib kelganini for check 
       res.send("Home Page");
     } catch (err) {
        console.log("Error, goHome;", err);
@@ -13,6 +14,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 
 restaurantController.getLogin = (req: Request, res: Response) => {
     try {
+      console.log("getLogin");  //loyiha standarti. 
       res.send("Login Page");
     } catch (err) {
        console.log("Error, getLogin;", err);
@@ -21,6 +23,7 @@ restaurantController.getLogin = (req: Request, res: Response) => {
 
 restaurantController.getSignup = (req: Request, res: Response) => {
     try {
+      console.log("getSignup");  //loyiha standarti. 
       res.send("Signup Page");
     } catch (err) {
        console.log("Error, getSignup;", err);
--- src/train.ts
+++ src/train.ts
@@ -1,18 +1,26 @@
-console.log("HELLO WORLD");
+/* Project Standards:
+   - Logging standards
+   - Naming standards
+       function, method, variable => CAMEL case
+       class => PASCAL case
+       folder => KEAB
+       css => SNAKE case 
+   - Error handling    
+*/
 
-//Task H
 
-function getPositive(arr: number[]) {
-    let result = "";
-    for (let num of arr) {
-        if (num > 0) {
-            result += num.toString();
-        }
-    }
-    return result;
-}
+// //Task H
+// function getPositive(arr: number[]) {
+//     let result = "";
+//     for (let num of arr) {
+//         if (num > 0) {
+//             result += num.toString();
+//         }
+//     }
+//     return result;
+// }
 
-console.log(getPositive([3, -6, 9]));  // Output: "12"
+// console.log(getPositive([3, -6, 9]));  // Output: "12"
 
 
 // // Task H
