c5c5736231f2dfbf7d3b09b0c96540f83aac0f04
Max <lazizyuldoshev93@gmail.com>
2024-04-09 01:38:46 +0900

    feat: develop verify authenticated  user credential

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -6,7 +6,7 @@ import {Request, Response} from "express";
 import {T} from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
-import Errors, { HttpCode } from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
 import { AUTH_TIMER } from "../libs/config";
 
@@ -48,8 +48,7 @@ memberController.login = async (req: Request, res: Response) => {
       maxAge: AUTH_TIMER * 3600 * 1000, 
       httpOnly: false,
     }); 
-     //TODO: TOKENS  AUTHENTICATIO
-
+     //TODO: TOKENS  AUTHENTICATion
     res.status(HttpCode.OK).json({member: result, accessToken: token});
   } catch (err) {
      console.log("Error, login;", err);
@@ -58,7 +57,20 @@ memberController.login = async (req: Request, res: Response) => {
   }   
 };
 
+memberController.verifyAuth = async (req: Request, res: Response) => {
+  try {
+    let member = null;
+    const token = req.cookies["accessToken"];
+    if (token) member = await authService.checkAuth(token)
 
+    if (!member) throw new Errors(HttpCode.UNAUTHORIZED, Message.NOT_AUTHENTICATED);
+    
+  } catch (err) {
+    console.log("Erro2r verifyAuth;", err);
+    if(err instanceof Errors ) res.status(err.code).json(err);
+     else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
 
 
 
--- src/router.ts
+++ src/router.ts
@@ -3,8 +3,20 @@ const router = express.Router();
 import memberController from './controllers/member.controller';
 import routerAdmin from "./router-admin";
 
-router.post("/login", memberController.login);
-router.post("/signup", memberController.signup);
+
+/** Member */
+router.post("/member/login", memberController.login);
+router.post("/member/signup", memberController.signup);
+router.get("member/detail", memberController.verifyAuth)
+
+
+
+/** Product  */
+
+
+
+/** Order */
+
 
 export default router;
 
