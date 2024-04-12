12405244149589760fb4b32274ffbee74fc430c8
Max <lazizyuldoshev93@gmail.com>
2024-04-13 03:01:59 +0900

    feat: develop getProduct rest Api

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -4,6 +4,7 @@ import Errors, { HttpCode, Message } from "../libs/Errors";
 import { Product, ProductInput, ProductInquiry, ProductUpdateInput } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 import { T } from "../libs/types/common";
+import {ObjectId} from "mongoose";
 
 class ProductService {
     private readonly productModel;
@@ -37,6 +38,25 @@ class ProductService {
         ])
         .exec();
         if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+        return result;
+    }
+
+    public async getProduct(
+        memberId: ObjectId | null, 
+        id: string
+    ): Promise<void> {
+       const productId = shapeIntoMongooseObjectId(id);
+       
+       let result = await this.productModel
+          .findOne({
+            _id: productId, 
+            productStatus: ProductStatus.PROCESS
+           })
+           .exec();
+        if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+            // TODO  If authenticated users => fist => view log creation
 
         return result;
     }
--- src/router.ts
+++ src/router.ts
@@ -14,24 +14,30 @@ router.post("/member/logout",
 memberController.verifyAuth, 
 memberController.logout
 );
-router.get("/member/detail", 
-memberController.verifyAuth, 
-memberController.getMemberDetail
+router.get(
+    "/member/detail", 
+    memberController.verifyAuth, 
+    memberController.getMemberDetail
 );
 
 
-router.post("/member/update", 
-memberController.verifyAuth, 
-uploader("members") .single("memberImage"),
-memberController.updateMember
+router.post(
+    "/member/update", 
+    memberController.verifyAuth, 
+    uploader("members") .single("memberImage"),
+    memberController.updateMember
 ); 
 
-
 router.get("/member/top-users", memberController.getTopUsers);
+
 
 /** Product  */
 
 router.get("/product/all", productController.getProducts);
+router.get("/product/:id", 
+  memberController.retrieveAuth,
+  productController.getProduct 
+);
 
 /** Order */
 
