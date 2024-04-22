d6d5b99fbe1bb5bd5aea4869fa33d2f240ccf8b8
Max <lazizyuldoshev93@gmail.com>
2024-04-22 18:57:39 +0900

    feat: MITASK ZC

--- src/train.ts
+++ src/train.ts
@@ -1,13 +1,27 @@
-//MITASK ZB
-/**type scriptda Shunday function yozing, uni 2 ta number parametri bolsin va
- *  berilgan sonlar orasidan random raqam return qilsin
-MASALAN: randomBetween(30, 50) return 45
- */
-function randomBetween(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+// MITASK ZC
+//type scriptda Shunday function yozing, uni number parametri bolsin va function qabul
+// parametrni selsiy miqdori sifatida qabul qilib uni farenhitga ozgartirib bersin
+//MASALAN: celsiusToFahrenheit(0) return 32
+
+function celsiusToFahrenheit(celsius: number): number {
+  return (celsius * 9) / 5 + 32;
 }
-const random: number = randomBetween(30, 50);
-console.log("randomBetween:", random);
+
+// Test
+console.log(celsiusToFahrenheit(0)); // 32
+
+//========================//
+
+// //MITASK ZB
+// /**type scriptda Shunday function yozing, uni 2 ta number parametri bolsin va
+//  *  berilgan sonlar orasidan random raqam return qilsin
+// MASALAN: randomBetween(30, 50) return 45
+//  */
+// function randomBetween(min: number, max: number): number {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// const random: number = randomBetween(30, 50);
+// console.log("randomBetween:", random);
 
 //===================//
 
